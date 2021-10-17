988684ede6f4414bdc6eb30749d630865b0abd50
Trump7 <trump@knights.ucf.edu>
2021-10-17 19:12:36 -0400

    44 Pseudocode, Junit, PlantUML

---
+++ exercise44/src/main/java/JsonSearcher.java
@@ -0,0 +1,19 @@
+import com.google.gson.JsonParser;
+
+public class JsonSearcher {
+    public boolean isNamePresent(String search){
+        //create nessacary parsers from the google gson library
+
+        //if the argument search is equal to one of the names in the json file
+        //return true
+        //else return false
+
+        return false;
+    }
+    public void printData(String search){
+        //go to the spot in the json file where the search name is
+        //print out the name
+        //print out the price
+        //print out the quantity
+    }
+}
---
+++ exercise44/src/main/java/Solution44.java
@@ -0,0 +1,13 @@
+public class Solution44 {
+    public static void main(String[] args) {
+        //create a variable isPresent to determine when to break out of the loop
+        //create a variable input to hold the users inputed string
+
+        //create a while loop that continues to loop until an item in the json file is found
+        //call isNamePresent with the user inputed string
+        //set isPresent equal to isNamePresent's returned value
+
+        //when the while loop breaks call printData from the JsonSearcher Class
+        //with the inputed string
+    }
+}
