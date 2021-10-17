f67a77605a984f26ab03d5fc156d4df77f1697f9
Trump7 <trump@knights.ucf.edu>
2021-10-17 15:39:20 -0400

    Pseudocode, PlantUML, and Tests

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -2,6 +2,8 @@ package baseline;
 
 public class Solution42 {
     public static void main(String[] args) {
-
+        //Call the readCSV function with the file location as the arguments
+        //Call the organizeData function to organize all data collected by readCSV function
+        //Call the exportData function with the file location as the arguments
     }
 }
--- exercise42/src/test/java/Solution42Test.java
+++ exercise42/src/test/java/Solution42Test.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Solution42Test {
 
     @Test
-    void testtest(){
+    void organizeTest(){
+        //create an arraylist with a csv type data
+        //set the new arraylist equal to the arraylist in the parseCSV class
+        //run the organizeData function
+        //compare the returned organized data with a string of the organized data
         //assertEquals();
     }
 }
